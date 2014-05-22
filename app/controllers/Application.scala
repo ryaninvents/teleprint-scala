92e7e9abf8e0101c900826b70b70707192ab506b
Ryan Muller <ryan.a.muller@gmail.com>
2014-05-22 19:43:06 -0400

    converted WebSocket to use JSON, yay!

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -4,6 +4,7 @@ import play.api._
 import play.api.mvc._
 import play.api.libs.iteratee.{Iteratee, Concurrent, Enumerator}
 import play.api.libs.concurrent.Execution.Implicits._
+import play.api.libs.json._
 
 object Application extends Controller {
 
@@ -11,16 +12,16 @@ object Application extends Controller {
     Ok(views.html.index("Your new application is ready."))
   }
 
-  def printer = WebSocket.using[String] { request =>
+  def printer = WebSocket.using[JsValue] { request =>
     //Concurrent.broadcast returns (Enumerator, Concurrent.Channel)
-    val (out,channel) = Concurrent.broadcast[String]
+    val (out,channel) = Concurrent.broadcast[JsValue]
 
     //log the message to stdout and send response back to client
-    val in = Iteratee.foreach[String] {
+    val in = Iteratee.foreach[JsValue] {
       msg => println(msg)
-             //the Enumerator returned by Concurrent.broadcast subscribes to the channel and will 
-             //receive the pushed messages
-             channel push("RESPONSE: " + msg)
+             (msg \ "time").asOpt[Long] match {
+               case Some(t) => if(t % 10 == 0) channel push(msg)
+             } 
     }
     (in,out)
   }
